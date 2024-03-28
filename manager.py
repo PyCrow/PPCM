b8fb9a7a2a33ee9de75417e060e1f8e7d557bf7a
Gregory Atamanov <g.atamanov@newinfosec.ru>
2024-03-28 20:15:49 +0300

    multitest (wip 1)

--- manager.py
+++ manager.py
@@ -1,7 +1,14 @@
+import sys
 from time import time
 from numpy.random import rand
 import matplotlib as mpl
 import matplotlib.pyplot as plt
+
+# Fixme: add execution code output as:
+# import inspect
+# Fixme: ...or...
+# s="str(1)"
+# lambda: eval(s)
 
 
 mpl.use('TkAgg')
@@ -14,7 +21,31 @@ FUNC_DATA = dict[str: DELTA_ARRAYS]
 class TestManager:
 
     def __init__(self):
-        self.__data: FUNC_DATA = dict()
+        self.__tests = dict()
+        self.__data = dict()
+        self.__current_test_name = None
+
+    def add_test(self, test, count: int = 1):
+        """
+        Add test to order.
+
+        :param test: Function, than runs all compared functions
+        :param count: Count of test runs improves the accuracy of comparison results
+        """
+        test_name = test.__name__
+        self.__tests[test_name] = (test, count)
+        self.__data[test_name] = {}
+
+    def run(self):
+        """ Run tests in order """
+        print("Tests were launched...")
+        for test_name, test_data in self.__tests.items():
+            self.__current_test_name = test_name
+            test, count = test_data
+            print(f"Test {test_name} started...")
+            for _ in range(count):
+                test()
+            print(f"Test {test_name} finished.")
 
     @staticmethod
     def get_min_max(deltas):
@@ -35,17 +66,16 @@ class TestManager:
             step = round(step * 1.6)
         return steps + [count]
 
-    def test(self, func, func_name: str = None,
-             count: int = 100):
+    def test(self, func, func_name: str = None, steps_count: int = 100):
         # Preparing
         if func_name is None:
             func_name = func.__name__
-        flag_steps = self.get_flag_steps(count)
+        flag_steps = self.get_flag_steps(steps_count)
         total_time = 0
         current_step = 0
         delta_for_step: DELTA_ARRAY = dict()
 
-        for _ in range(count):
+        for _ in range(steps_count):
             # Test
             start = time()
             func()
@@ -58,8 +88,10 @@ class TestManager:
                 delta_for_step[current_step] = total_time / current_step
 
         # Saving current func delta array
-        self.__data[func_name] = \
-            self.__data.get(func_name, []) + [delta_for_step]
+        test_name = self.__current_test_name
+        if func_name not in self.__data[test_name]:
+            self.__data[test_name][func_name] = []
+        self.__data[test_name][func_name].append(delta_for_step)
 
     @staticmethod
     def _total_average_values(
@@ -74,17 +106,30 @@ class TestManager:
             y.append(sum(delta_arrays[i][step] for i in range(arrays_count)))
         return x, y
 
-    def show(self):
-        if not self.__data:
-            print("No test were run")
-            return
+    def _add_plot(self):
+        ...
 
-        for func_name, delta_for_step in self.__data.items():
-            x, y = self._total_average_values(delta_for_step)
-            plt.plot(x, y, color=rand(3,), label=func_name)
-        plt.xlabel("Step")
-        plt.ylabel("Delta")
-        plt.ticklabel_format(style='plain')
-        plt.title("Delta for step")
-        plt.legend()
+    def show(self):
+        if not self.__current_test_name:
+            raise Exception("No test were run!")
+        if not self.__data:
+            raise Exception("Test were run, but data not found!")
+        if not any([bool(test_data) for test_data in self.__data.values()]):
+            raise Exception("Test data not found!")
+
+        fig, axs = plt.subplots(1)
+        for test_index, test_name in enumerate(self.__data):
+            for func_name, delta_for_step in self.__data[test_name].items():
+                x, y = self._total_average_values(delta_for_step)
+                func_color = rand(3,)
+                axs.plot(x, y, color=func_color, label=func_name)
+            axs.set(xlabel='Step', ylabel='Delta (sec)')
+            axs.set_title(test_name)
+            axs.ticklabel_format(style='plain')
+            axs.get_yaxis().get_major_formatter().set_useOffset(False)
+            axs.legend()
+
+        plt.suptitle(f"All tests | Python {sys.version}")
         plt.show()
+        # Fixme: add removing test data for saving temp result
+        plt.close()
